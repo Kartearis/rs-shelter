a526eee8cc2044a992f4b9aa57eed15e0f64a173
Timofey Matveev <justshowel@gmail.com>
2022-04-26 01:59:49 +0300

    feat: Implement popups. Currently not adaptive.

--- shelter/pages/pets.js
+++ shelter/pages/pets.js
@@ -1,7 +1,9 @@
 
 import Pagination from "../modules/pagination.js";
+import Popup from "../modules/popup.js";
 
 window.addEventListener("DOMContentLoaded", () => {
     let pageContainer = document.querySelector("[data-pagination]");
     const pagination = new Pagination(pageContainer);
+    document.querySelectorAll(".card").forEach(x => new Popup(x));
 });
